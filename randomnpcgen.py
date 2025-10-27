95f0c76c4cf47f6642644e9f555d9686f11695a9
Shiven <shiven.sharma5111@gmail.com>
2025-10-27 17:40:04 +0000

    msg get better

---
+++ randomnpcgen.py
@@ -0,0 +1,22 @@
+import random
+import time
+import math
+
+usernames = []
+
+for i in range(10):
+    usernames.append(input("Gimmie Names:"))
+
+personality = [
+    "accepting", "adaptable", "adventurous", "ambitious", "analytical", "brave",
+    "calm", "caring", "charismatic", "clever", "compassionate", "confident", 
+    "considerate", "courageous", "creative", "curious", "decisive", "dedicated",
+    "diligent", "disciplined", "easygoing", "empathetic", "energetic", "enthusiastic",
+    "fearless",  "flexible", "forgiving", "friendly", "funny", "generous", "grateful",
+    "hard-working", "honest", "humble", "imaginative", "independent", "industrious",
+    "intelligent", "kind", "logical", "loyal", "mature", "observant", "optimistic",
+    "patient", "perceptive", "persistent", "resourceful", "responsible", "sincere"
+]
+
+
+
