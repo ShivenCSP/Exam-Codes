c260285152c4c856ae76a9b4c0ad72e796ca2009
Shiven <shiven.sharma5111@gmail.com>
2025-10-30 15:55:07 +0000

    mdh

--- randomnpcgen.py
+++ randomnpcgen.py
@@ -7,7 +7,7 @@ import math
 usernames = []
 
 for i in range(10):
-    usernames.append(input("Gimmie Names:"))
+    usernames.append(input("Give me 10 Names:"))
 
  # list for attributes and jobs of the npc
 personality = [
@@ -32,23 +32,32 @@ trades = [
 for name in usernames:
     traits = random.sample(personality, 2)
     job = random.choice(trades)
+    age = random.randint(18, 65)
 
 # attempt to add bool and floats
 
     mage = bool(random.randint(0, 1))
     skill = round(random.uniform(0.0, 10.0), 2)
+    height = round(random.uniform(1.7, 2.0), 2)
 
 # prints everything in 0.2 second intrevals
 
-    print(f"\n\nName: {name}")
+    print("\n\n____________________________________")
+    print(f"Name: {name}")
+    time.sleep(0.2)
+    print(f"Age: {age} years old")
+    time.sleep(0.2)
+    print(f"Height: {height} meters tall")
     time.sleep(0.2)
     print(f"Job: {job}")
     time.sleep(0.2)
-    print(f"Traits: {", ".join(traits)}.")
+    print(f"Skill Level: {skill}")
+    time.sleep(0.2)
+    print(f"Traits: {", ".join(traits)}.") # uses space and comma to join traits gramatically
     time.sleep(0.2)
     print(f"Magic User: {mage}")
     time.sleep(0.2)
-    print(f"Job skill Level: {skill}")
-    time.sleep(0.2)
+    print("____________________________________")
+    time.sleep(0.5)
 
 # made by the magnificient shiven
